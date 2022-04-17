6be7b8aaf103ebe80998c7075ee39146223e732a
Cody2022 <cody.zheng@hotmail.com>
2022-04-16 20:53:48 -0600

    weather widget

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+import "./Footer.css"
+
+const Footer = (props) => {
+    const setShowFooter=props.setShowFooter;
+    const [temp, setTemp]=useState();
+    const [description, setDescription]=useState();
+    const [icon, setIcon]=useState();
+    const [city, setCity]=useState("chengde");
+    const [country, setCountry]=useState();
+
+    // useEffect(()=>{
+    //     const geoLocation=async ()=>{
+    //         let geoResponse=await fetch("https://freegeoip.app/json/")
+    //         let geoJson=await geoResponse.json();
+    //         setCity(geoJson.city);
+    //     };
+    //       geoLocation();        
+    //     },[]
+    // )
+         const geoLocation=async ()=>{
+                let geoResponse=await fetch("https://freegeoip.app/json/")
+                let geoJson=await geoResponse.json();
+                setCity(geoJson.city);
+            };
+          geoLocation(); 
+
+
+    const fetchWeather=async ()=>{  
+            const response= await fetch('/api/weather',{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json",
+                },
+                body:JSON.stringify({cityName:city})
+            });
+            console.log("response status", response.status)
+            const weatherData=await response.json()
+            const { main, sys, weather } = weatherData;
+            // setShowFooter(true);
+            setIcon(`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`);
+            setDescription(weather[0]["description"]);
+            setCountry(sys.country); 
+            setTemp(Math.round(main.temp));
+        };
+          fetchWeather();
+    
+    //  console.log("temp is", temp);
+  return (
+    <div className="footer">
+        <div className="canvas">
+              <span>{city}</span><sup>{country}</sup><br/>
+              <span className="city-temp">{temp}<sup>°C</sup></span>
+              <figure className='icon'>
+                   <img className="weather-icon" src={icon} alt={description}/>
+                    <figcaption>{description}</figcaption>
+                </figure> 
+            <br/>
+         </div> 
+    </div>
+  );
+}
+
+export default Footer
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,42 @@
-import React from "react";
+import React,{useState}from "react";
 import { Outlet, Link } from "react-router-dom";
 import background from "../../img/background.jpg";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
+  const [showFooter, setShowFooter]=useState(true)
+  
   return (
-    <div
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundRepeat: "no-repeat",
-        height: 750,
-        width: 1600,
-      }}
-    >
-      <div className="home">
-        <ul>
-          <li>
-            <Link to="/login" style={{ color: "black" }}>
-              Login
-            </Link>
-          </li>
-          <li>
-            <Link to="/signup" style={{ color: "black" }}>
-              Signup
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: "no-repeat",
+          height: 700,
+          width: 1600,
+        }}
+      >
+          <div className="home">
+            <ul>
+              <li>
+                <Link to="/login" style={{ color: "black" }}>
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link to="/signup" style={{ color: "black" }}>
+                  Signup
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-      <Outlet />
+          <Outlet />
+         </div>
+          {/* <Footer /> */}
+       {showFooter && <Footer setShowFooter={setShowFooter}/>}
     </div>
-  );
+    );
 };
 
 export default Home;
