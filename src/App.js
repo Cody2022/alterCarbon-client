762ee59d8ad2392abe06855bd88b795e935803f6
Zach Odio <zachariaodio@gmail.com>
2022-04-21 21:25:04 -0600

    Minor formatting footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/LoginPage/LoginPage';
