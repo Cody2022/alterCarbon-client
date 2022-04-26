5daee0c3cb29d09a2519a8335e9f337d0b27a62c
Siri <sirilakku01@gmail.com>
2022-04-26 14:51:43 -0600

    Added map route("/map")

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-
-import React, { useEffect } from 'react';
-import {Routes, Route } from 'react-router-dom';
-import './App.css';
-import LoginPage from './components/LoginPage/LoginPage';
-import SignupPage from './components/SignupPage/SignupPage';
-import Home from './components/Home/Home';
-import HistoryPage from './components/HistoryPage/HistoryPage';
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./components/LoginPage/LoginPage";
+import SignupPage from "./components/SignupPage/SignupPage";
+import Home from "./components/Home/Home";
+import HistoryPage from "./components/HistoryPage/HistoryPage";
+import Maps from "./components/mapBox";
 
 function App() {
-  
-  useEffect(()=>{document.title="Home"},[])
-  
+  useEffect(() => {
+    document.title = "Home";
+  }, []);
+
   return (
     <div className="wrapper">
-       <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<LoginPage />}/>
-            <Route exact path="/signup" element={<SignupPage />}/>
-            <Route exact path="/history" element={<HistoryPage />}/>
-        </Routes>
-      
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/signup" element={<SignupPage />} />
+        <Route exact path="/history" element={<HistoryPage />} />
+        <Route exact path="/map" element={<Maps />} />
+      </Routes>
     </div>
   );
 }
