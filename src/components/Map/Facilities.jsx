e6526864e1586c190f4a20e7e9adac75d5c54a1d
Cody2022 <cody.zheng@hotmail.com>
2022-05-02 01:05:46 -0600

    Cody_Added carbon footprint button on Popup

--- src/components/Map/Facilities.jsx
+++ src/components/Map/Facilities.jsx
@@ -13,7 +13,6 @@ import Map, {
 import ControlPanel from './ControlPanel';
 import Pin from './pin';
 import "./Map.css"
-
 import FacilitiesData from '../Data/electricity.json';
 
 const TOKEN = process.env.REACT_APP_MAPBOX_TOKEN; // Set your mapbox token here
@@ -22,10 +21,9 @@ export default function Facilities() {
   const [popupInfo, setPopupInfo] = useState(null);
   const [year, setYear]=useState(2020);
   const [month, setMonth]=useState("Dec");
-
-  // console.log("year is:",year, typeof year);
-  // console.log("Month is:",month, typeof month);
-  
+  const [showEmission,setShowEmission]=useState(false);
+  const [electricityCO2,setElectricityCO2]=useState();
+ 
   const FacilitiesDataYearMonth=FacilitiesData.filter(facility=>facility.year===`${year}` && facility.month===`${month}`)
 
   const pins = useMemo(
@@ -48,23 +46,39 @@ export default function Facilities() {
       ))
   );
 
+    const handleButton=async (e)=>{
+      e.preventDefault();
+      const response = await fetch("/api/electricity", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({electricity:popupInfo.total_consumption}),
+      });
+      if (response.status===200) {
+          const data=await response.json();
+          setShowEmission(true);
+          setElectricityCO2(data.electricityCO2);
+      }
+    }
+
   return (
     <div id='map'>
       <Map
-        initialViewState={{
-          longitude: -114.027608,
-          latitude: 51.045614,
-          zoom: 10,
-          bearing: 0,
-          pitch: 0,
-        }}
-        mapStyle="mapbox://styles/mapbox/outdoors-v11?optimize=true"
-        mapboxAccessToken={TOKEN}
+          initialViewState={{
+            longitude: -114.027608,
+            latitude: 51.045614,
+            zoom: 10,
+            bearing: 0,
+            pitch: 0,
+          }}
+          mapStyle="mapbox://styles/mapbox/outdoors-v11?optimize=true"
+          mapboxAccessToken={TOKEN}
       >
-        <GeolocateControl position="top-left" />
-        <FullscreenControl position="top-left" />
-        <NavigationControl position="top-left" />
-        <ScaleControl />
+          <GeolocateControl position="top-left" />
+          <FullscreenControl position="top-left" />
+          <NavigationControl position="top-left" />
+          <ScaleControl />
         
         {pins}
         
@@ -73,12 +87,17 @@ export default function Facilities() {
             anchor="top"
             longitude={Number(popupInfo.longitude)}
             latitude={Number(popupInfo.latitude)}
-            onClose={() => setPopupInfo(null)}
+            onClose={() => {setPopupInfo(null); setShowEmission(false)}}
+            onOpen={()=>{setShowEmission(false)}}
+            style={{border:"solid", borderColor:"#4110efe0"}}
+            
           >
-            <div style={{fontVariant: "small"}}>
+            <div style={{backgroundColor:"rgba(211, 250, 0, 0.4)", fontVariant: "small"}}>
               <p>{"Facility name: "}{popupInfo.facilityname}</p>
-              <p>Facility Address: {popupInfo.facilityaddress} <br/>{' '}{`${popupInfo.month}, ${popupInfo.year}`}</p>
+              <p>Facility Address: {popupInfo.facilityaddress} <br/>{`Date: `}{`${popupInfo.month}, ${popupInfo.year}`}</p>
               <p>{`${popupInfo.energy_description}: ${popupInfo.total_consumption} ${popupInfo.unit}`}</p>
+              <button onClick={handleButton}>Carbon footprint</button>
+              {showEmission&&electricityCO2 && <p>CO2 Equivalent, kg: {electricityCO2} <br/> Offset: {Math.ceil(electricityCO2/60)} tree(s) seedlings grown for 10 years</p>}   
             </div>
             
           </Popup>
@@ -90,6 +109,4 @@ export default function Facilities() {
   );
 }
 
-// export function renderToDom(container) {
-//   render(<Facilities />, container);
-// }
+
