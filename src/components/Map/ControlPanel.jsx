ff37834d497fe544033abeeb84b01b10c099cabc
Cody2022 <cody.zheng@hotmail.com>
2022-05-02 01:25:40 -0600

    Cody_Adjusted Map.css

--- src/components/Map/ControlPanel.jsx
+++ src/components/Map/ControlPanel.jsx
@@ -13,7 +13,7 @@ const BootstrapInput = styled(InputBase)(({ theme }) => ({
     marginTop: theme.spacing(3),
   },
   '& .MuiInputBase-input': {
-    borderRadius: 4,
+    borderRadius: 10,
     position: 'relative',
     backgroundColor: theme.palette.background.paper,
     border: '1px solid #ced4da',
@@ -34,7 +34,7 @@ const BootstrapInput = styled(InputBase)(({ theme }) => ({
       '"Segoe UI Symbol"',
     ].join(','),
     '&:focus': {
-      borderRadius: 4,
+      borderRadius: 10,
       borderColor: '#80bdff',
       boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
     },
