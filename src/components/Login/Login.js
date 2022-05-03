9590c0f3e373e0d82cb69e3335bedb06ef83f406
Siri <sirilakku01@gmail.com>
2022-05-02 19:42:56 -0600

    Updated react page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
+import loginImg from "../../img/LoginBackground.png";
+import { Button, TextField } from "@mui/material";
 
 const loginUser = async (credentials) => {
   const response = await fetch("/api/login", {
@@ -31,42 +33,52 @@ const Login = (props) => {
   const navigate = useNavigate();
 
   return (
-    <div className="login-wrapper">
+    <div
+      style={{
+        backgroundImage: `url(${loginImg})`,
+        backgroundRepeat: "no-repeat",
+        height: 700,
+        width: 1510,
+      }}
+    >
+    <div className="login-form">
       <h1>Please Log In</h1>
-      <form className="login-form" onSubmit={handleSubmit}>
-        {message && (
+        <form onSubmit={handleSubmit}>
+        
+          {message && (
+            <div>
+              <label style={{ color: "red" }}>{message}!</label>
+            </div>
+          )}
+          <br />
+            <TextField
+            label = 'Name'
+            variant= "standard"
+            type="text"
+            name="Username"
+            placeholder="Username"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <br />
+          <br />
+           <TextField
+            label = 'Password'
+            variant= "standard"
+            type="password"
+            name="Password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <div>
-            <label style={{ color: "red" }}>{message}!</label>
             <br />
+            <br />
+            <Button type="submit" style={{color:"white",backgroundColor:"#04AA6D"}}>Log in</Button>
+            
           </div>
-        )}
-        <br />
-        <label>Username:</label>
-        <br />
-        <input
-          type="text"
-          name="Username"
-          placeholder="Username"
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <br />
-
-        <label>Password: </label>
-        <br />
-        <input
-          type="password"
-          name="Password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        </form>
         <div>
           <br />
-          <button type="submit">Log in</button>
-          <br />
         </div>
-      </form>
-      <div>
-        <br />
       </div>
     </div>
   );
